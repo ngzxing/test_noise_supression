b7a90ed33488c767523519356c6af00eb2415420
ngzxing <92634733+ngzxing@users.noreply.github.com>
2022-03-20 17:20:31 +0800

    Update noisereducer.py

--- noisereducer.py
+++ noisereducer.py
@@ -3,8 +3,8 @@ from scipy.io import wavfile
 
 def noise_sup(infile, outfile):
     
-    rate, data = wavfile.read("C:\\Users\\user\Downloads\\"+infile)
+    rate, data = wavfile.read(infile)
     reduced_noise = nr.reduce_noise(y=data, sr=rate,prop_decrease=0.95)
-    wavfile.write("C:\\Users\\user\Downloads\\"+outfile, rate, reduced_noise)
+    wavfile.write(outfile, rate, reduced_noise)
     
-noise_sup("babble_15db.wav", "babbleReduced.wav")
+)
